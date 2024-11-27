76403ccc6ff6ea146c5e00df9d699bbb1e6b4ecc
Asumakn <143426903+Asumakn@users.noreply.github.com>
2024-11-27 10:31:54 -0500

    Update UserManagement.sh
    Added input validation

--- components/UserManagement.sh
+++ components/UserManagement.sh
@@ -3,58 +3,72 @@
 select option in addUser giveRootPermission deleteUser showUsers disconnectUser showUserGroups changeUserGroups Exit
 do
 
-	case $option in
-	addUser)
-	echo "enter Username"
-	read name
-	echo "enter User password"
-	read password
-	sudo useradd -p $password $name
-	echo "User $name has been added"
-	;;
-
-	giveRootPermission)
-	echo "Enter Username"
-	read name
-	sudo usermod -aG root $name
-	;;
-
-	deleteUser)
-	echo "Enter Username"
-	read name
-	sudo userdel -f $name
-	echo "User $name has been deleted"
-	;;
-
-	showUsers)
-	who|awk '{print $1}'
-	;;
-
-	disconnectUser)
-	who -u
-	echo "Enter user code"
-	read code
-	kill $code
-	;;
-
-	showUserGroups)
-	echo "Enter Username"
-	read name
-	sudo groups $name
-	;;
-
-	changeUserGroups)
-	echo "Enter Username"
+        case $option in
+        addUser)
+        echo "enter Username"
         read name
+        sudo useradd  $name 2> /dev/null
+        echo "enter User password"
+        sudo passwd $name
+        if [ $? -ne 0 ]
+        then
+        echo "input was invalid"
+        else
+        echo "User $name has been added"
+        fi
+        ;;
+        giveRootPermission)
+        echo "Enter Username"
+        read name
+        $name ALL=(ALL:ALL)ALL
+        ;;
+        deleteUser)
+        echo "Enter Username"
+        read name
+        sudo userdel -f $name 2> /dev/null
+        if [ $? -ne 0 ]
+        then
+        echo "input was invalid"
+        else
+        echo "User $name has been deleted"
+        fi
+        ;;
+
+        showUsers)
+        who|awk '{print $1}'
+        ;;
+
+        disconnectUser)
+        who -u
+        echo "Enter user code"
+        read code
+        kill $code 2> /dev/null
+        if [ $? -ne 0 ]
+        then
+        echo "input was invalid"
+        else
+        echo "User has been disconnected"
+        fi
+        ;;
+        changeUserGroups)
         echo "Enter the current Group"
         read group1
-	echo "Enter the new Group"
-	read group2
-	sudo groupmod $group1  $group2
-	;;
-	Exit)
-	break
-	;;
+        echo "Enter the new Group"
+        read group2
+        sudo groupmod $group1  $group2 2> /dev/null
+        if [ $? -ne 0 ]
+        then
+        echo "input was invalid"
+        else
+        echo " $group1 groups have been changed"
+        fi
 
-	esac
+        ;;
+        Exit)
+        break
+        ;;
+
+        esac
 done
+
+
