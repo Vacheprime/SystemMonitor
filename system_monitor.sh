1b8b26b8f88f9806af868b74dcc58c1fcde14d98
florencekn <2380334@edu.vaniercollege.qc.ca>
2024-11-27 12:05:31 -0500

    Made changes to the menu

--- system_monitor.sh
+++ system_monitor.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
-# Define a list of options the user can chose from
-options=("System Status" "Backup" "Network" "Services" "User Management" "File Management" "Exit")
+# Define some color codes for easier use
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+RESET='\e[0m'
 
-# Create a menu containing all of the options from the options variable
-select option in "${options[@]}"
-do
-	# Check which option the user chose
-	case option in
-		"System Status")
-			# Execute system status script
-			# Example: bash components/test.sh
-			;;
-		"Backup")
-			# Execute backup script
-			;;
-		"Network")
-			# Execute network script
-			;;
-		"Services")
-			# Execute services script
-			;;
-		"User Management")
-			# Execute user management script
-			;;
-		"File Management")
-			# Execute file management script
-			;;
-		*)
-			# Default statement code
-			;;
-	esac
+# Function for CMD1 Submenu
+submenu() {
+    while true; do
+        echo -e "\n${GREEN}CMD1 SUBMENU${RESET}"
+        echo -e "${CYAN}Choose an option: ${RESET}"
+        options=("Go Back to Main Menu" "System Status" "Backup" "Network" "Services" "User Management" "File Management" "Exit")
+        select option in "${options[@]}"; do
+            case $REPLY in
+                1) return ;;                                            # Return to Main Menu
+                2)	chmod +x systemStatus.sh
+			./systemStatus.sh ;;                 		# System Status
+                3) # Backup File
+                4)	chmod +x Network.sh
+			./Network.sh                                    # Network
+                5) # System Services
+                6) ./UserManagement.sh
+                7) ./fileManagement.sh
+                8) exit 0 ;;                                            # Exit the script
+                *) echo -e "${RED}Invalid option. Try again.${RESET}"; break ;;
+            esac
+        done
+    done
+}
+
+# Main Menu
+while true; do
+    echo -e "\n${PURPLE}MAIN MENU${RESET}"
+    echo -e "${CYAN}Choose an option: ${RESET}"
+    options=("CMD1" "Exit")
+    select option in "${options[@]}"; do
+        case $REPLY in
+            1) submenu; break ;;  # Call CMD1 Submenu
+            2) exit 0 ;;          # Exit the script
+            *) echo -e "${RED}Invalid option. Try again.${RESET}"; break ;;
+        esac
+    done
 done
