0d71eeda7fb2c8ae21bbade667328b1bd4b588e0
Asumakn <143426903+Asumakn@users.noreply.github.com>
2024-11-27 10:46:31 -0500

    Update Network.sh

--- components/Network.sh
+++ components/Network.sh
@@ -1,41 +1,57 @@
 #!/bin/bash
-echo "select an opiton"
+echo "select an option"
 select options in ShowList Disable Enable SetIPaddress SelectNetwork Exit
-	do
-		case $options in
-		ShowList)
-		ifconfig | grep  -e"mtu" -e"inet"
-		;;
-		Disable)
-		echo "enter card name "
-		read name
-		sudo ifconfig $name down
-		;;
-		Enable)
-		echo "enter card name"
-		read name
-		sudo ifconfig $name up
-		;;
-		SetIPaddress)
-		echo "enter  an IPaddress "
-		read IP
-		echo "enter Net Card"
-		read Netcard
-		sudo ifconfig $Netcard $IP
-		;;
-		SelectNetwork)
-		echo "List of networks"
-		nmcli dev wifi | awk '{print $2}'> networks.txt
-		uniq -u networks.txt
-		echo "enter network"
-		read $netName
-		echo "enter password"
-		read pass
-                nmcli dev wifi connect $netName  password $pass
-		rm -r networks.txt
-		;;
-		Exit)
-		break
-	;;
-	esac
+        do
+                case $options in
+                ShowList)
+                ifconfig | grep  -e"mtu" -e"inet"
+                ;;
+                Disable)
+                echo "enter card name "
+                read name
+                sudo ifconfig $name down 2> /dev/null
+                 if [ $? -ne 0 ]
+                then
+                echo "input is invalid"
+                fi
+                ;;
+                Enable)
+                echo "enter card name"
+                read name
+                sudo ifconfig $name up  2> /dev/null
+                if [ $? -ne 0 ]
+                then
+                echo "input is invalid"
+                fi
+                ;;
+                SetIPaddress)
+                echo "enter  an IPaddress "
+                read IP
+                echo "enter Net Card"
+                read Netcard
+                sudo ifconfig $Netcard $IP 2> /dev/null
+                if [ $? -ne 0 ]
+                then
+                echo "inputs are invalid"
+                fi
+                ;;
+                SelectNetwork)
+                echo "List of networks"
+                nmcli dev wifi | awk '{print $2}'> networks.txt
+                uniq -u networks.txt
+                echo "enter network"
+                read netName
+                echo "enter password"
+                read pass
+                nmcli dev wifi connect $netName  password $pass 2> /dev/null
+                if [ $? -ne 0 ]
+                then
+                echo "inputs are invalid"
+                fi
+                ;;
+
+                Exit)
+                break
+                ;;
+        esac
 done
