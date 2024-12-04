7a986fa9a45f37124c0a5769c96a2e59259731fb
Danat <danatmuradov500@gmail.com>
2024-12-03 19:51:44 -0500

    Developed the display services and stop service functionality.

--- components/services.sh
+++ components/services.sh
@@ -1,3 +1,49 @@
 #!/bin/bash
 
+function stopService() {
+    echo -e "\nAll active services: \n"
+    # Get all active services
+    allServices=$(systemctl list-units --state=active --type=service --quiet | # list all inactive services
+	awk '$2 == "loaded" {print $1}' | # Select only those that are loaded and can be enabled
+	sed "s/.service//" # Remove trailing .service
+    )
+    # Display  the services as columns
+    echo -e "$allServices" | column
+    echo ""
 
+    read -p "Enter a service to stop: " service
+    if ! echo "$allServices" | grep -P "^$service$" &> /dev/null; then
+	echo -e "\nYou must enter a service from the list of inactive services!\n"
+	return 1
+    fi
+    if sudo systemctl stop "$service"; then
+	echo -e "\nService '$service' stopped!"
+    else
+	echo -e "\nCould not stop service '$service'!"
+    fi
+    echo ""
+}
+
+PS3=$'\nSelect an option: '
+select option in "Show Current Services" "Stop a Service" "Start a Service" "Go Back to Main Menu"
+do
+    case "$option" in 
+	"Show Current Services")
+	    # List all active services
+	    systemctl list-units --state=active --type=service | less -S
+	    ;;
+	"Start a Service")
+	    echo ""
+	    ;;
+	"Stop a Service")
+	    stopService
+	    ;;
+	"Go Back to Main Menu")
+	    exit 0
+	    ;;
+	*)
+	    echo -e "\nPlease enter a valid option!"
+	    ;;
+    esac
+    REPLY=
+done
