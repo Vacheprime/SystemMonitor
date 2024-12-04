fd82ef50e3331bfc242360fe8209d9a26e483003
florencekn <2380334@edu.vaniercollege.qc.ca>
2024-12-03 20:29:42 -0500

    Added colors to fileManagement, still need to test if the options can have colors

--- components/fileManagement.sh
+++ components/fileManagement.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Colors:
+CYAN=$(tput setaf 6)
+PURPLE=$(tput setaf 5)
+BLUE=$(tput setaf 4)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+
 # NOTE: Searching a file in the user's home directory will in a function,
 # and the unsername validation will be outside  of the function
 
@@ -9,12 +18,12 @@ findFileInHomeDir(){
 # New Steps: use sudo find to recursively search for the given file
 
 # 3. If the home directory is not readable, then use sudo find to search for the file
-   read -p "Enter the name of the file: " filename
+   read -p "${PURPLE}Enter the name of the file: ${RESET}" filename
 	   found_file=$(sudo find "$1" -type f -name "$filename" 2>/dev/null | head -n 1)
 	   if [ -n "$found_file" ]; then
-		echo "File found: $found_file"
+		echo "${GREEN}File found: $found_file ${RESET}"
 	   else
-		echo "Error: File '$filename' not found in $1"
+		echo "${RED}Error: File '$filename' not found in $1 ${RESET}"
 	  fi
 }
 
@@ -26,13 +35,15 @@ findFileInHomeDir(){
         # %n: name of the full path
 # {}: ensures that '-exec' knows where to insert the path of the file. 'stat' would not know which file to act upon without it.
 displayTenLargestFiles(){
-   echo "10 Largest Files: "
+   echo "${CYAN}10 Largest Files ${RESET}"
+   echo "${PURPLE}SIZE (MB) | FULL PATH OF THE FILE ${RESET}"
 	find "$1" -type f ! -path "$1/$username/.dbus" -exec stat --format="%s %n" {} + 2>/dev/null | sort -rh | head
 }
 
 #• Display the 10 oldest files in the user's home directory.
 displayTenOldestFiles(){
-   echo "10 Oldest Files: "
+   echo "${CYAN}10 Oldest Files ${RESET}"
+   echo "${PURPLE}LAST DATE OF MODIFICATION | FULL PATH OF THE FILE ${RESET}"
 	# %T+: Prints the last modification time of the file (format: year-month-day+hours:minutes:seconds)
 	# %p: Prints the full path of the file
 	# 2>dev/null: Suppress the permission denied message, even when the command prints the expected results
@@ -45,29 +56,29 @@ sendFileAsEmailAttachment(){
 	# This will be used in case the user does not have the mutt command installed
 	command_installer=$"./installCommand.sh"
 
-	read -p "Enter the email address: " email
-	read -p "Enter the file name to send as an email attachment: " filename
+	read -p "${CYAN}Enter the email address: ${RESET}" email
+	read -p "${PURPLE}Enter the file name to send as an email attachment: ${RESET}" filename
 
 	#Validate the email format using regex
 	if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
 		# Check if the mutt command is installed
                 if ! which mutt > /dev/null; then
                 	if [ ! -f "$command_installer" ]; then
-        	                echo "Error: Command installer script '$command_installer' does not exists"
+        	                echo "${RED}Error: Command installer script '$command_installer' does not exists${RESET}"
                                 return 1
                         fi
 
                         # Install the mutt command using the installer script
                         $command_installer mutt &> /dev/null
                         if [ $? -ne 0 ]; then
- 	                       echo "Failed to install 'mutt' command. Exiting..."
+ 	                       echo "${RED}Failed to install 'mutt' command. Exiting...${RESET}"
                                return 1
                         fi
                 fi
 
 		#Check if the file exists
 		if [ -f "$1/$filename" ]; then
-			echo "Sending the file as an email attachment..."
+			echo "${GREEN}Sending the file as an email attachment...${RESET}"
 			# echo before the pipe: will be written inside the body of the email
 			# mutt command (mail command did not work): tool to read and send emails
 			# -s option: specifies the subject of the email (Sending File filename)
@@ -78,13 +89,13 @@ sendFileAsEmailAttachment(){
 
 			# Error Handling: Check the exit status to see if program ran successfully
 			if [ $? -eq 0 ]; then
-				echo "File sent successfully."
+				echo "${GREEN}File sent successfully.${RESET}"
 			else
-				echo "File not found in the directory."
+				echo "${RED}File not found in the directory.${RESET}"
 			fi
 		fi
 	else
-		echo "Invalid email address. Please enter an email with a valid format."
+		echo "${RED}Invalid email address. Please enter an email with a valid format.${RESET}"
 	fi
 }
 
@@ -92,10 +103,10 @@ sendFileAsEmailAttachment(){
 
 # Accept the username as input
 while true; do
-read -p "Enter the username (or enter 'Exit' to leave the program): " username
+read -p "${CYAN}Enter the username (or enter 'Exit' to leave the program): ${RESET}" username
 
 if [[ "$username" == "Exit" ]]; then
-   echo "Exiting Program."
+   echo "${YELLOW}Exiting the Program...${RESET}"
    exit 0
 fi
 # Check if the user exists
@@ -103,14 +114,13 @@ if id "$username" &>/dev/null; then
    # If the username exists, get the home directory by extracting it from /etc/passwd
    # -d option with cut command: Seperate the fields of the etc/passwd with the colon delimeter
    # f6: extract the home directory ( which is the 6th field inside etc/passwd)
-   home_dir=$(grep "^$username:" /etc/passwd | cut -d: -f6)
-	echo "Testing home directory: $home_dir"
+   home_dir=$(grep "^$username:" /etc/passwd | cut -d: -f6
    # Check if the home directory exists
    if  [ -d "$home_dir" ]; then
 	# Run this menu until the use chooses to exit
 	while true; do
 	# Prompt the user with message asking to choose an option in the file management program
-	PS3="Please select an option: "
+	PS3="${BLUE}Please select an option: ${RESET}"
 	select option in "Search For a File" "Find the 10 Largest Files" "Find the 10 Oldest Files" "Send File as An Email Attachment" "Exit";
 
 	    do
