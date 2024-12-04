3219f34fd888034d479e1b7b47955bda95757e5f
Danat <danatmuradov500@gmail.com>
2024-12-04 10:30:36 -0500

    Fixed syntax errors and formatting.

--- components/UserManagement.sh
+++ components/UserManagement.sh
@@ -17,9 +17,8 @@ do
         echo -e "\e[1;32m\n User $name has been added \n\e[0m"
         fi
         echo -e "\e[1;34m ____________________________ "
-        echo -e "\e[1;34m Enter another Option Number | "
-        echo -e "\e[1;34m ---------------------------- "
-        echo -e "\e[1;34m1) addUser             3) deleteUser          5) disconnectUser      7) changeUserGroups\n2) giveRootPermis>
+        echo -e "\e[1;34m |Enter another Option Number | "
+        echo -e "\e[1;34m ---------------------------- \n"
         ;;
         giveRootPermission)
         echo -e "\e[1;37m\nEnter Username"
@@ -32,9 +31,9 @@ do
         echo -e "\e[1;32m\n User $name has been given root permissons \n\e[0m"
         fi
         echo -e "\e[1;34m ____________________________"
-        echo -e "\e[1;34m Enter another Option Number |"
-        echo -e "\e[1;34m ----------------------------"
-        echo -e "\e[1;34m 1) addUser             3) deleteUser          5) disconnectUser      7) changeUserGroups\n2) giveRootPermi>        ;;
+        echo -e "\e[1;34m |Enter another Option Number |"
+        echo -e "\e[1;34m ----------------------------\n"
+	;;
         deleteUser)
         echo "Enter Username"
         read name
@@ -46,17 +45,16 @@ do
         echo -e "\e[1;32m\n User $name has been removed \n\e[0m"
         fi
         echo -e "\e[1;34m ____________________________ "
-        echo -e "\e[1;34m Enter another Option Number | "
-        echo -e "\e[1;34m ---------------------------- "
-        echo -e "\e[1;34m1) addUser             3) deleteUser          5) disconnectUser      7) changeUserGroups\n2) giveRootPermis>        ;;
+        echo -e "\e[1;34m |Enter another Option Number | "
+        echo -e "\e[1;34m ----------------------------\n"
+	;;
 
         showUsers)
         echo -e "\e[1;37m"
         who|awk '{print $1}'
         echo -e "\e[1;34m ___________________________"
-        echo -e "\e[1;34m Enter another Option Number|"
-        echo -e "\e[1;34m ---------------------------"
-        echo -e "\e[1;34m1) addUser             3) deleteUser          5) disconnectUser      7) changeUserGroups\n2) giveRootPermis>        ;;
+        echo -e "\e[1;34m |Enter another Option Number|"
+        echo -e "\e[1;34m ---------------------------\n"
         ;;
         disconnectUser)
         who -u
@@ -70,9 +68,8 @@ do
         echo -e "\e[1;32m\n User $name has been disconnected  \n\e[0m"
         fi
         echo -e "\e[1;34m ___________________________"
-        echo -e "\e[1;34m Enter another Option Number|"
-        echo -e "\e[1;34m ---------------------------"
-        echo -e "\e[1;34m1) addUser             3) deleteUser          5) disconnectUser      7) changeUserGroups\n2) giveRootPermis>        ;;
+        echo -e "\e[1;34m |Enter another Option Number|"
+        echo -e "\e[1;34m ---------------------------\n"
         ;;
         changeUserGroups)
         echo "Enter the current Group"
@@ -87,7 +84,8 @@ do
         echo -e "\e[1;32m\n User $name group has beem changed  \n\e[0m"
         fi
         echo -e "\e[1;34m ___________________________"
-        echo -e "\e[1;34m Enter another Option Number|"
+        echo -e "\e[1;34m |Enter another Option Number|"
+        echo -e "\e[1;34m ---------------------------\n"
         ;;
         disconnectUser)
         who -u
@@ -101,13 +99,12 @@ do
         echo -e "\e[1;32m\n User $name has been disconnected  \n\e[0m"
         fi
         echo -e "\e[1;34m ___________________________"
-        echo -e "\e[1;34m Enter another Option Number|"
-        echo -e "\e[1;34m ---------------------------"
-        echo -e "\e[1;34m1) addUser             3) deleteUser          5) disconnectUser      7) changeUserGroups\n2) giveRootPermis>        ;;
+        echo -e "\e[1;34m |Enter another Option Number|"
+        echo -e "\e[1;34m ---------------------------\n"
+	;;
         changeUserGroups)
         echo "Enter the current Group"
-        read group1
-        echo "Enter the new Group"
+        read group1 echo "Enter the new Group"
         read group2
         sudo groupmod $group1  $group2 2> /dev/null
         if [ $? -ne 0 ]
@@ -117,13 +114,14 @@ do
         echo -e "\e[1;32m\n User $name group has beem changed  \n\e[0m"
         fi
         echo -e "\e[1;34m ___________________________"
-        echo -e "\e[1;34m Enter another Option Number|"
-        echo -e "\e[1;34m ---------------------------"
-        echo -e "\e[1;34m1) addUser             3) deleteUser          5) disconnectUser      7) changeUserGroups\n2) giveRootPermis>        ;;
+        echo -e "\e[1;34m |Enter another Option Number|"
+        echo -e "\e[1;34m ---------------------------\n"
+	;;
         Exit)
         break
            ;;
 
         esac
+	REPLY=
 done
 
